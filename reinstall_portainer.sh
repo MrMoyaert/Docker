71ad043c45a635e2d7786a9c10948eb7c4ea83f6
Ruben Moyaert <ruben.moyaert@gmail.com>
2023-09-06 07:14:56 +0200

    Update reinstall_portainer.sh

--- reinstall_portainer.sh
+++ reinstall_portainer.sh
@@ -33,4 +33,4 @@ echo Pruning unused volume.  If asked answer yes.
 sudo docker volume prune
 
 echo now doing a fresh install of portainer
-sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest --logo "https://pi-hosted.com/pi-hosted-logo.png" || error "Failed to execute newer version of Portainer!"
+sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to execute newer version of Portainer!"
